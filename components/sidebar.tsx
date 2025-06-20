0e8ea64a47db1b2b0858cf1cc7505cfe5e643abe
Yi Heng <mail@ohyiheng.com>
2025-06-20 17:15:33 +0800

    Add basic sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,70 @@
+import clsx from "clsx"
+import Link from "next/link"
+import { btnStyles } from "@/components/button"
+import { Search } from "iconoir-react"
+
+export default function Sidebar({
+    children, collapsed
+}: Readonly<{
+    children: React.ReactNode, collapsed?: boolean | null
+}>) {
+    return (
+        <div className={`
+            ${collapsed && "-ml-(--sidebar-width)"}
+            w-(--sidebar-width) min-h-screen h-full
+            p-3 space-y-4
+            bg-neutral-100
+            border-r border-r-neutral-200
+            duration-200 ease-out
+        `}>
+            {children}
+        </div>
+    )
+}
+
+export function SidebarLink({
+    href,
+    title,
+    icon,
+}: Readonly<{
+    href: string,
+    title: string,
+    icon?: any,
+}>) {
+    return (
+        <Link href={href} className={clsx(
+            btnStyles.base,
+            btnStyles.layout.full,
+            btnStyles.color.transparent,
+            "w-full block"
+        )}>
+            {icon ?
+                <div title={title} className="flex items-center gap-1.5 truncate">
+                    {icon}
+                    {title}
+                </div>
+                :
+                <div title={title} className="truncate">
+                    {title}
+                </div>
+            }
+
+        </Link>
+    )
+}
+
+export function SidebarSearch() {
+    return (
+        <button className={clsx(
+            btnStyles.base,
+            btnStyles.layout.full,
+            btnStyles.color.white,
+            "rounded-full"
+        )}>
+            <div className="flex items-center justify-center gap-2">
+                <Search />
+                Search
+            </div>
+        </button>
+    )
+}
