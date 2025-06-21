8fd3177c179f7c385d06e3eb19df984c853f3c49
Yi Heng <mail@ohyiheng.com>
2025-06-21 12:50:45 +0800

    feat: Add project-tree display support on sidebar

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx"
 
 export const btnStyles = {
-    base: "font-medium rounded-md cursor-pointer duration-75 ease-in-out border",
+    base: "font-medium rounded-md cursor-pointer duration-50 ease-in-out border",
     layout: {
         normal: "py-1.5 px-3",
         full: "py-1.5 px-3 w-full",
@@ -9,8 +9,8 @@ export const btnStyles = {
     },
     color: {
         primary: "bg-sky-600 dark:bg-sky-400 text-neutral-50 dark:text-neutral-900 hover:bg-sky-700 dark:hover:bg-sky-500 border-transparent",
-        white: "bg-white dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700 hover:border-neutral-500 text-neutral-600 hover:text-neutral-900 dark:text-neutral-300 dark:hover:text-neutral-50",
-        transparent: "border-transparent hover:bg-neutral-500/10",
+        white: "bg-white dark:bg-neutral-800 border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 dark:hover:border-neutral-500 text-neutral-600 hover:text-neutral-900 dark:text-neutral-300 dark:hover:text-neutral-50",
+        transparent: "border-transparent hover:bg-neutral-500/10 dark:hover:bg-neutral-400/20",
         transparentWithBorder: "border-neutral-200 hover:bg-neutral-500/10",
     }
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [],
+    theme: {
+        extend: {
+            transitionDuration: {
+                "50": "50ms"
+            }
+        },
+    },
+    plugins: [],
+} satisfies Config
