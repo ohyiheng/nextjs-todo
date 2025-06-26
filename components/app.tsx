bc8e574e8e03dc9d967f2f9df7490ae608e416d7
Yi Heng <me@ohyiheng.com>
2025-06-26 18:33:51 +0800

    refactor: Combine ActiveTask contexts into a single provider

--- components/app.tsx
+++ components/app.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { ProjectNode } from "@/lib/definitions";
-import React, { useContext, useState } from "react";
+import React from "react";
 import { Sidebar } from "@/components/sidebar";
-import { btnStyles } from "@/components/button";
-import { X } from "lucide-react";
 import { TaskContainer } from "./task";
-import clsx from "clsx";
 import { ProjectContext } from "./context/ProjectContext";
+import { ActiveTaskProvider } from "./context/ActiveTaskContext";
 import { useTitle } from "./context/TitleContext";
+import RightPanel from "./right-panel";
+import Header from "./header";
 
 
 export default function AppLayout({
@@ -22,9 +22,7 @@ export default function AppLayout({
     return (
         <div className="flex h-screen max-h-screen box-border">
             <ProjectContext value={projects}>
-                <ActiveTaskProvider>
-                    {children}
-                </ActiveTaskProvider>
+                {children}
             </ProjectContext>
         </div>
     )
@@ -34,35 +32,15 @@ export function App() {
     const { title, setTitle } = useTitle();
 
     return (
-        <>
+        <ActiveTaskProvider>
             <Sidebar setTitle={setTitle} />
-            <main className="grow bg-neutral-50 dark:bg-neutral-950 flex flex-col">
-                <header className="p-(--outer-padding) w-full flex justify-between items-center">
-                    <h1 className="text-2xl">{title}</h1>
-                    <div></div>
-                </header>
+            <main className="grow bg-zinc-50 dark:bg-zinc-950 flex flex-col">
+                <Header title={title} />
                 <div className="p-4 mx-auto w-full grow">
                     <TaskContainer />
                 </div>
             </main>
-            <div className={clsx(
-                !activeTask && "-mr-(--task-details-width)",
-                "w-(--task-details-width) h-full p-(--outer-padding)","border-l border-neutral-300 bg-white",
-                "duration-200 ease-in-out"
-            )}>
-                <div className="flex items-center gap-3">
-                    <button
-                        className={clsx(
-                            btnStyles.base,
-                            btnStyles.layout.icon,
-                            btnStyles.color.transparentWithBorder
-                        )}
-                        onClick={() => { setActiveTask(null) }}>
-                        <X />
-                    </button>
-                    <h2 className="font-semibold text-lg">{activeTask?.name}</h2>
-                </div>
-            </div>
-        </>
+            <RightPanel />
+        </ActiveTaskProvider>
     )
 }
