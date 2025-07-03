17affaaf294d38e9c3e5c0febc5c13fb1c4f2804
Yi Heng <mail@ohyiheng.com>
2025-07-03 12:35:52 +0800

    feat: Add ability to edit tasks (except for completing it)

--- components/app/date-picker.tsx
+++ components/app/date-picker.tsx
@@ -6,26 +6,30 @@ import { Calendar } from "../ui/calendar";
 import { CalendarIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { format } from "date-fns";
+import { UseFormSetValue } from "react-hook-form";
+import { TaskFormType } from "./task-form";
 
 export function DatePicker({
     initialDate,
+    onChange,
     size = "default"
 }: {
-    initialDate: Date | undefined,
+    initialDate: Date | null,
+    onChange: (...event: any[]) => void,
     size?: "default" | "sm"
 }) {
-    const [ date, setDate ] = useState<Date | undefined>(initialDate);
+    // const [ date, setDate ] = useState<Date | undefined>(initialDate);
 
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button variant="outline" size={size} className="cursor-pointer font-normal text-sm" onClick={e => e.stopPropagation()}>
                     <CalendarIcon strokeWidth="1.5px" className="p-[1.5px]" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    {initialDate ? format(initialDate, "PPP") : <span>Pick a date</span>}
                 </Button>
             </PopoverTrigger>
             <PopoverContent onClick={e => e.stopPropagation()} className="p-0 w-full">
-                <Calendar buttonVariant="outline" mode="single" required={false} selected={date} onSelect={setDate} captionLayout="dropdown" />
+                <Calendar buttonVariant="outline" mode="single" required={false} selected={initialDate ?? undefined} onSelect={onChange} captionLayout="dropdown" />
             </PopoverContent>
         </Popover>
     )
