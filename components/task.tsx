1c7822ef75e93889c6ecc5019817e703cdeb3e9d
Yi Heng <me@ohyiheng.com>
2025-07-03 13:41:00 +0800

    feat: Add collapsible section for completed tasks

--- components/task.tsx
+++ components/task.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import clsx from "clsx";
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { TaskNode } from "@/lib/definitions";
 import { TasksContext, TasksDispatchContext } from "./providers/TasksContext";
 import { DatePicker } from "./app/date-picker";
-import { Edit2 } from "lucide-react";
+import { ChevronRight, Edit2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import TaskForm from "./app/task-form";
 import { Checkbox } from "./ui/checkbox";
 import { completeTask } from "@/lib/actions";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
+import { partition } from "@/lib/utils";
 
 export function Task({
     id,
@@ -37,28 +39,28 @@ export function Task({
 
     return (
         <Sheet>
-            <SheetTrigger asChild>
-                <div
+            <div
+                className={clsx(
+                    "bg-zinc-100 dark:bg-zinc-900",
+                    "relative",
+                    "flex items-center gap-4",
+                    "px-4 py-2",
+                    "border",
+                    "rounded-lg cursor-pointer",
+                    "group",
+                )}
+            >
+                <Checkbox checked={taskNode.completed}
+                    onCheckedChange={async () => {
+                        console.log(taskNode.completed);
+                        await completeTask(taskNode.id, taskNode.completed);
+                        taskNode.completed = !taskNode.completed;
+                    }}
                     className={clsx(
-                        "bg-zinc-100 dark:bg-zinc-900",
-                        "relative",
-                        "flex items-center gap-4",
-                        "px-4 py-2",
-                        "border",
-                        "rounded-lg cursor-pointer",
-                        "group",
-                    )}
-                >
-                    <Checkbox checked={taskNode.completed}
-                        onCheckedChange={async () => {
-                            console.log(taskNode.completed);
-                            await completeTask(taskNode.id, taskNode.completed);
-                            taskNode.completed = !taskNode.completed;
-                        }}
-                        className={clsx(
-                            "size-5 rounded-full",
-                            completeBtnStyle
-                        )} />
+                        "size-5 rounded-full",
+                        completeBtnStyle
+                    )} />
+                <SheetTrigger asChild>
                     <div className="grow flex items-center gap-2">
                         <p className={clsx(
                             "font-semibold",
@@ -68,8 +70,8 @@ export function Task({
                             <DatePicker size="sm" initialDate={taskNode.dueDate} />
                         }
                     </div>
-                </div >
-            </SheetTrigger>
+                </SheetTrigger>
+            </div >
             <SheetContent className="w-screen md:w-[700px] h-full">
                 <SheetHeader>
                     <SheetTitle>Edit Task</SheetTitle>
@@ -84,17 +86,43 @@ export function Task({
 
 export function TaskContainer() {
     const tasks = useContext(TasksContext);
-    const dispatch = useContext(TasksDispatchContext);
     if (tasks == null) {
         return;
     }
+    const [ pendingTasks, completedTasks ] = partition(tasks, task => !task.completed);
+    const [ open, setOpen ] = useState(false);
 
     return (
-        <TaskSection>
-            {tasks.map(task => (
-                <Task key={task.id} id={task.id} taskNode={task} />
-            ))}
-        </TaskSection>
+        <div className="space-y-6">
+            {pendingTasks.length > 0 &&
+                <TaskSection>
+                    {tasks.filter(task => !task.completed).map(task => (
+                        <Task key={task.id} id={task.id} taskNode={task} />
+                    ))}
+                </TaskSection>
+            }
+            {completedTasks.length > 0 &&
+                <Collapsible open={open} onOpenChange={setOpen}>
+                    <TaskSection>
+                        <CollapsibleTrigger>
+                            <div className="flex items-center gap-1 cursor-pointer">
+                                <p className="text-sm text-muted-foreground font-medium">Completed</p>
+                                <ChevronRight className={clsx(
+                                    "stroke-muted-foreground",
+                                    "duration-100 ease-in-out",
+                                    open && "rotate-90"
+                                )} />
+                            </div>
+                        </CollapsibleTrigger>
+                        <CollapsibleContent>
+                            {tasks.filter(task => task.completed).map(task => (
+                                <Task key={task.id} id={task.id} taskNode={task} />
+                            ))}
+                        </CollapsibleContent>
+                    </TaskSection>
+                </Collapsible>
+            }
+        </div>
     )
 }
 
