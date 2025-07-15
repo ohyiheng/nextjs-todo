716f66445c8835ff795d3b7956d7935ba307f129
Yi Heng <me@ohyiheng.com>
2025-07-15 14:54:07 +0800

    refactor: Move inbox to /app/inbox from /app

--- app/app/inbox/page.tsx
+++ app/app/inbox/page.tsx
@@ -1,7 +1,6 @@
 import { fetchTasks } from "@/lib/data";
 import TasksProvider from "@/components/providers/TasksContext";
 import { TaskContainer } from "@/components/task";
-import { SortingProvider } from "@/components/providers/SortingProvider";
 
 export default async function Page() {
     let tasks = await fetchTasks();
@@ -9,9 +8,7 @@ export default async function Page() {
 
     return (
         <TasksProvider tasks={tasks}>
-            <SortingProvider>
-                <TaskContainer />
-            </SortingProvider>
+            <TaskContainer />
         </TasksProvider>
     )
 }
