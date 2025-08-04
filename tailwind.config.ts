dccbfa446886e76773120f516a22a7100ce2ecb4
Yi Heng <mail@ohyiheng.com>
2025-08-04 22:22:52 +0800

    chore: Update config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-    content: [],
+    content: [
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
     theme: {
         extend: {
             transitionDuration: {
