bcf37a232918e770472972547441f072e2d420ed
Yi Heng <me@ohyiheng.com>
2025-08-04 22:00:56 +0800

    refactor: Decouple more options dropdown from app title

--- components/app/app-title.tsx
+++ components/app/app-title.tsx
@@ -2,33 +2,23 @@
 
 import { activeProjectAtom, activeTagAtom } from "@/lib/atoms";
 import { useAtomValue } from "jotai";
+import { Hash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import ProjectDropdown from "./project/project-dropdown";
-import useTags from "../providers/TagsProvider";
-import { Button } from "../ui/button";
-import { Ellipsis } from "lucide-react";
-import useProjects from "../providers/ProjectsProvider";
-import TagDropdown from "./tag/tag-dropdown";
-
 
 export default function AppTitle() {
     const activeProject = useAtomValue(activeProjectAtom);
     const activeTag = useAtomValue(activeTagAtom);
-    const { inboxId } = useProjects();
-    const tags = useTags();
     const pathname = usePathname();
 
-    let title;
+    let title: string | undefined;
     if (activeProject) {
         title = activeProject.name;
+    } else if (activeTag) {
+        title = activeTag;
     } else {
-        title = pathname.split('/')[ 2 ];
-        if (title === "tag") {
-            title = pathname.split('/')[ 3 ] ?? "Tag not found"
-        } else {
-            title = title.replace(title[ 0 ], title[ 0 ].toLocaleUpperCase());
-        }
+        title = pathname?.split('/')[ 2 ];
+        title = title?.replace(title[ 0 ], title[ 0 ].toLocaleUpperCase());
     }
 
     const [ mounted, setMounted ] = useState(false);
@@ -42,26 +32,11 @@ export default function AppTitle() {
     }
 
     return (
-        <div className="flex items-center gap-3 truncate">
-            <h1 className="text-lg md:text-xl lg:text-2xl truncate">{title}</h1>
-            {activeProject && activeProject?.id !== inboxId &&
-                <ProjectDropdown project={activeProject}>
-                    <Button variant="ghost" size="icon" onClick={(e) => {
-                        e.stopPropagation();
-                    }}>
-                        <Ellipsis />
-                    </Button>
-                </ProjectDropdown>
-            }
-            {activeTag &&
-                <TagDropdown tag={activeTag}>
-                    <Button variant="ghost" size="icon" onClick={(e) => {
-                        e.stopPropagation();
-                    }}>
-                        <Ellipsis />
-                    </Button>
-                </TagDropdown>
-            }
+        <div className="lg:absolute lg:left-1/2 lg:-translate-x-1/2 max-w-3/5 flex items-center gap-3 truncate">
+            <h1 className="flex items-center gap-1 text-lg md:text-xl lg:text-2xl truncate">
+                {activeTag && <Hash />}
+                {title ?? "Tugas"}
+            </h1>
         </div>
     )
 }
